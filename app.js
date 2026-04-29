663d538603575f17dff8de5da51cb1a57c5a85fb
PraviN3003hwcwbUgfcq <pravinyadav9926@gmail.com>
2026-04-29 18:05:49 +0530

    done

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(
     cors({
         origin: 'https://resi-hub.onrender.com',
         // origin: `${process.env.URL_FRONTEND}`,
-        // origin: 'http://localhost:5173',
+        origin: 'http://localhost:5173',
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE" ,"PATCH"],
         allowedHeaders: ["Content-Type", "Authorization"],
